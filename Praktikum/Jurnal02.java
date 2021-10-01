9c49d9dccc6d756ddbe3dff83e72425f8611afd1
ihdazul <ihdazul123@gmail.com>
2021-10-01 07:05:11 +0700

    jurnal.java

---
+++ Praktikum/Jurnal02.java
@@ -0,0 +1,8 @@
+public class Jurnal02 {
+
+	public static void main(String[] args) {
+		System.out.println("NIM: 6706213042");
+		System.out.println("Nama: Ihdazul Aruna Syahriar");
+		System.out.println("Kelas: 45-04");
+	}
+}
